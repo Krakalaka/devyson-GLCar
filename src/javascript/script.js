db573f19b3fa5e43e2e04291aa3fc34719310744
Rafael Alvarenga <rafaelalvarengabarbosa@gmail.com>
2025-11-17 20:22:11 -0300

    Added index.html, header, hero section, about section and gallery section

---
+++ src/javascript/script.js
@@ -0,0 +1,30 @@
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+}
+
+function slideShow(n) {
+    let   slides  = document.getElementsByClassName("slide");
+    const slidesL = slides.length;
+
+    if (n > slidesL) {galleryIndex = 1      ;}
+    if (n < 1      ) {galleryIndex = slidesL;}
+
+    for (let i = 0; i < slidesL; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[galleryIndex - 1].style.display = "block";
+}
+
+function plusSlide(n) {
+    galleryIndex += n;
+    slideShow(galleryIndex);
+}
+
+function automaticSlide() {
+    galleryIndex += 1;
+    slideShow(galleryIndex);
+    setTimeout(automaticSlide, 3000);
+}
+
+let galleryIndex = 1;
+automaticSlide();
